61ed7014a62c3aca34855f7937b9290c78def8a9
Poretskov Oleg <porezkov@mail.ru>
2020-04-26 13:06:19 +0500

    Initial commit

---
+++ android/src/libgdx/stargame/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package libgdx.stargame;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import libgdx.stargame.StarGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StarGame(), config);
+	}
+}
